44644eb4c6748780178af0d625a5ebb34bdc5147
wandersonav <wandersonav07@gmail.com>
2023-04-18 22:05:29 -0300

    alteração

--- mochila-de-viagem/mochila-de-viagem/js/main.js
+++ mochila-de-viagem/mochila-de-viagem/js/main.js
@@ -16,16 +16,26 @@ evento.preventDefault()
 const nome = evento.target.elements['nome']
 const quantidade = evento.target.elements['quantidade']
 
-
 const itemAtual = {
     "nome":nome.value,                                    //Criando um objeto que contém nome e quantidade
     "quantidade":quantidade.value
 }
 
-criaElemento(itemAtual);
+const existe = itens.find(elemento => elemento.nome === nome.value); //Buscando o elemento nome no array, para saber se ele existe
+   
+//console.log(existe);
 
+if(existe){
+itemAtual.id = existe.id
+}else{
 
-itens.push(itemAtual);  //Pego o item e coloco no array item.
+    itemAtual.id = itens.lenght // item vai receber o ID do tamanho que estiver o array
+
+    criaElemento(itemAtual) // cria o item
+    itens.push(itemAtual)  //Pego o item e coloco no array item.
+    
+}
+
 
 localStorage.setItem("itens",JSON.stringify(itens))  //O JSON.stringify tranforma o nosso objeto em texto.
 
@@ -33,19 +43,23 @@ nome.value = ""
 quantidade.value = ""
 }) 
 
+
+
 function criaElemento(item){
 
-    console.log(nome);
-    console.log(quantidade);
+    //console.log(nome);
+   // console.log(quantidade);
 
     //<li class="item"><strong>7</strong>Camisas</li>
 
     const novoItem = document.createElement('li');
     novoItem.classList.add("item");
   
-  
+
+
     const numeroItem = document.createElement('strong');
     numeroItem.innerHTML = item.quantidade;
+    numeroItem.dataset.id = item.id;
 
     novoItem.appendChild(numeroItem)   //Os elemento criados via JavaScript são objetos que devem ser manipulados como objeto através do appendChild
     novoItem.innerHTML += item.nome;
